aeec6d3a4a95eff685d1a172f0bfb947ec54bd97
ocueye <carsonmayn@fuse.net>
2024-10-06 19:13:38 +0000

    fixed port

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class ZipApp:
 if __name__ == '__main__':
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',  # Bind to all available network interfaces
-        'server.socket_port': 8080,
+        'server.socket_port': 90,
         'log.screen': True,
     })
     cherrypy.quickstart(ZipApp(), '/', {'/': {'tools.sessions.on': True}})
